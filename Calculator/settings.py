2ea7cff3d9a9f067e3f2955d503a1b1ca2e597d6
CruzJeff <26846343+CruzJeff@users.noreply.github.com>
2018-01-05 12:35:46 -0500

    Update settings.py

--- Calculator/settings.py
+++ Calculator/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEST_RUNNER = 'testing.DatabaselessTestRunner'
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -116,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = STATICFILES_DIRS
